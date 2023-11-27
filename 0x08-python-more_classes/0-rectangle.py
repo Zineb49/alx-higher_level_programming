f3a5e05a39fa3276d327752c03dafa3ca6629781
Zineb49 <lahrizizineb1@gmail.com>
2023-11-27 11:02:54 -0800

    second try

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""
-module "0-rectangle"
+"""0-rectangle, built for Holberton project 0x08 task 0.
 """
 
 
 class Rectangle:
+    """Empty class per task instructions, will be built upon in later tasks.
+    """
     pass
